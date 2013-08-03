f37a37979810b418c09c58ad106c551baedb8ae4
dosimont <damien.dosimont@imag.fr>
2013-08-03 23:08:20 +0200

    fair

--- lpaggregtest/src/LPAggregNodeTest.cpp
+++ lpaggregtest/src/LPAggregNodeTest.cpp
@@ -51,44 +51,50 @@ int main(int argc, const char* argv[]){
 
     	//Gain/loss parameter
     	float parameter = atof((*loop)[3].c_str());
-
+    	int number = (*loop).size()-4;
     	//Vector for storing trace elements
     	vector<NodeLPAggreg*>elements;
     	vector<NodeLPAggreg*>leaves;
     	vector<NodeLPAggreg*>orphans;
     	vector<NodeLPAggreg*>nonorphans;
+
+    	NodeLPAggreg* root=new NodeLPAggreg(0,0);
+    	elements.push_back(root);
+
+    	for (int i=0; i<number-1; i++){
+    		int father=min((int)((double)(rand())/(double)(RAND_MAX)*((double)elements.size()-1)),(int)elements.size()-1);
+    		elements.push_back(new NodeLPAggreg(i+1,0));
+    		elements[father]->addChild(elements[elements.size()-1]);
+    	}
+
     	//Store input file line elements in the vector
 
-    	for (unsigned int i = 4; i<(*loop).size(); i++){
-    		elements.push_back(new NodeLPAggreg(i-4,atof((*loop)[i].c_str())));
-    		if (i!=4)
-    			orphans.push_back(elements[elements.size()-1]);
-    		else
-    			nonorphans.push_back(elements[elements.size()-1]);
-    	}
-
-    	while (!orphans.empty()){
-    		int son= ((double)(rand())/(double)RAND_MAX)*((double)orphans.size());
-    		int father= ((double)(rand())/(double)RAND_MAX)*((double)nonorphans.size());
-    		if (son<(int)orphans.size()&&father<(int)nonorphans.size()&&orphans[son]!=nonorphans[father]&&!(orphans[son]->ownsNode(nonorphans[father]))){
-    			nonorphans[father]->addChild(orphans[son]);
-    			nonorphans.push_back(orphans[son]);
-    			orphans.erase(orphans.begin()+son);
-    		}
-    	}
+//    	for (unsigned int i = 4; i<(*loop).size(); i++){
+//    		//elements.push_back(new NodeLPAggreg(i-4,atof((*loop)[i].c_str())));
+//    		//orphans.push_back(elements[elements.size()-1]);
+//    	}
+//
+//    	while (orphans.size()>1){
+//    		int son= ((double)(rand())/(double)RAND_MAX)*((double)orphans.size());
+//    		int father= ((double)(rand())/(double)RAND_MAX)*((double)elements.size());
+//    		if (son<(int)orphans.size()&&father<(int)elements.size()&&(!orphans[son]->ownsNode(elements[father]))){
+//    			elements[father]->addChild(orphans[son]);
+//    			//elements.push_back(orphans[son]);
+//    			orphans.erase(orphans.begin()+son);
+//    		}
+//    	}
 
     	//LPAggreg Scalar algorithm is called to process the data
-    	NodeLPAggreg * algo=NULL;
+    	NodeLPAggreg * algo=root;
 
     	for (unsigned int i=0; i< elements.size()-1; i++){
     		if (!elements[i]->hasChild())
     			leaves.push_back(elements[i]);
-    		if (!elements[i]->hasParent()){
-    			algo = elements[i];
-    		}
     	}
 
-
+    	for (unsigned int i=0; i< leaves.size()-1; i++){
+    		leaves[i]->setValue(atof((*loop)[i].c_str()));
+    	}
 
     	//Init -> compute qualities
     	algo->init(false);
